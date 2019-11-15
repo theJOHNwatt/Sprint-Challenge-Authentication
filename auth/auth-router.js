c5e951871d1506292828d1a5010add24305770e1
theJOHNwatt <j.watt10@gmail.com>
2019-11-15 09:24:23 -0800

    added an auth-helpers for validating a user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Auth = require('./auth-model.js');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  
+  
 });
 
 router.post('/login', (req, res) => {
