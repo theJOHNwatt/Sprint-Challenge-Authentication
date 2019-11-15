67c21a15794ffb3a2443ea4f0d0e03b2bcaa052b
theJOHNwatt <j.watt10@gmail.com>
2019-11-15 09:22:07 -0800

    added an auth-model

---
+++ auth/auth-model.js
@@ -0,0 +1,23 @@
+const db = require('../database/dbConfig.js');
+
+module.exports = {
+    add,
+    findBy,
+    findById
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user);
+
+    return findById(id);
+}
+
+function findBy(filter) {
+    return db('users')
+        .where({id})
+        .first();
+}
+
+function findBy(filter) {
+    return db('users').where(filter);
+}
