6b8224010db09bbaf2aa65788a90d38c8794e5fd
theJOHNwatt <j.watt10@gmail.com>
2019-11-15 10:51:17 -0800

    tests for register and login add

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -17,7 +17,7 @@ router.post('/register', (req, res) => {
 
     Auth.add(user)
       .then(saved => {
-        res.status(201).json(saved);
+        res.status(201).json({registered: `Added ${user.username}`});
       })
       .catch(error => {
         res.status(500).json({error: 'User added with an error'});
