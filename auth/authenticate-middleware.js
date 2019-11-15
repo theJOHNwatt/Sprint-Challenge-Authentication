eae2555ce7920b22ecef4d01225fc86084377288
theJOHNwatt <j.watt10@gmail.com>
2019-11-15 09:59:46 -0800

    Login and register routes tested and working. jokes is restricted and needs authorization to access with jwt token

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,26 +2,26 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (token) {
-    const secret = process.env.JWT_SECRET || "supersecret";
+    const secret = process.env.JWT_SECRET || 'supersecret';
 
     
     jwt.verify(token, secret, (err, decodedToken) => {
       if (err) {
         
-        res.status(401).json({ message: "You shall not pass!" });
+        res.status(401).json({ message: 'You shall not pass!' });
       } else {
         req.decodedJwt = decodedToken;
         next();
       }
     });
   } else {
-    res.status(400).json({ message: "No credentials provided" });
+    res.status(400).json({ message: 'No credentials provided' });
   }
 };
 
